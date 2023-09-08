0bd5ea726a9ac869ab025bf0dabff06eaa795661
KatiaLosignore <katialos.kl@gmail.com>
2023-09-08 18:54:19 +0200

    added bonus

--- csharp-oop-shop/Product.cs
+++ csharp-oop-shop/Product.cs
@@ -33,40 +33,49 @@ namespace csharp_oop_shop
 
         // METODI
 
-        // funzione che genera un un numero Random tra 1 e 999999
+        // genero un un numero Random tra 1 e 999999
         public int GetCodeRandom()
         {
             Random randomNumber = new Random();
             return randomNumber.Next(1, 999999);
         }
 
-        // funzione che genera il prezzo base
+        // genero il prezzo base
         public string PriceBase()
         {
             return this.Price + " " + "Euro";
         }
 
-        // funzione che genera il prezzo base comprensivo di iva
+        // genero il prezzo base comprensivo di iva
         public decimal PriceVatIncluded()
         {
 
             return Decimal.Round(((this.Price / 100) * this.Vat) + Price, 2);
         }
 
-        // funzione che restituisce il codice con un pad left di 0 per arrivare a 8 caratteri 
+        // restituisce il coddice prodotto con un pad left di 0 per arrivare a 8 caratteri 
 
         public string GetCodeWithPadLeft()
         {
             return this.Code.ToString().PadLeft(8, '0');
         }
 
-        // funzione che restituisce il nome esteso, ottenuto concatenando codice + nome
+        // restituisco il nome esteso, ottenuto concatenando codice prodotto + nome
 
         public string ExtendedName()
         {
             return GetCodeWithPadLeft() + " " + this.Name;
         }
 
+        // stampo i dati presenti nel prodotto
+
+        public void PrintProductInfo()
+        {
+            Console.WriteLine($"The Product code and name : {ExtendedName()}");
+            Console.WriteLine($"The Product description : {this.Description}");
+            Console.WriteLine($"The base price of the product : {PriceBase()}");
+            Console.WriteLine($"The price of the product with VAT included: {PriceVatIncluded()} Euro. (Iva applicata al {this.Vat} %)");
+        }
        
     }
 }
--- csharp-oop-shop/Program.cs
+++ csharp-oop-shop/Program.cs
@@ -2,7 +2,9 @@
 
 using csharp_oop_shop;
 
+/*
 Product myProduct = new Product("Scatoletta di tonno", "Rio mare garantisce un marchio di qualità al prodotto");
+
 
 myProduct.Price = 100;
 Console.WriteLine(myProduct.PriceBase());
@@ -14,3 +16,33 @@ Console.WriteLine(myProduct.GetCodeWithPadLeft());
 Console.WriteLine(myProduct.PriceVatIncluded());
 
 Console.WriteLine(myProduct.ExtendedName());
+
+myProduct.PrintProductInfo();
+*/
+
+// Istanzio 5 prodotti della classe Product
+
+Product penne = new Product("Penne", "Pasta much appreciated for its lightness.", 8.00M);
+Product rigatoni = new Product("Rigatoni", "Rough texture that holds the condiments better.", 10.00M);
+Product paccheri = new Product("Paccheri", "Thanks to their large cavity they pair particularly well with full-bodied sauces and ragù.", 15.00M);
+Product fusilli = new Product("Fusilli", "Type of pasta with an elongated shape having two or more coils, made with durum wheat semolina.", 12.00M);
+Product linguine = new Product("Linguine", "they are a long, flat pasta shape that goes well with pesto.", 9.00M);
+
+
+// inserisco i 5 prodotti istanziati all'interno di un Array
+
+Product[] arrayProducts = { penne, rigatoni, paccheri, fusilli, linguine };
+
+Console.WriteLine("Products list: ");
+
+
+// ciclo i prodotti e stampo l'elenco
+
+for (int i  = 0; i < arrayProducts.Length; i++)
+{
+    Console.WriteLine();
+
+    Console.WriteLine($"Product {i + 1}:");
+
+    arrayProducts[i].PrintProductInfo();        
+}
